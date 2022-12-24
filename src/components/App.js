a6753e15817b08525afb648db7e317f9ca623742
Rutvik11102000 <101519046+Rutvik11102000@users.noreply.github.com>
2022-12-24 19:24:46 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,22 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
+    switch(event.detail){
+      case 1:{
+        console.log("single click")
+      }
+      case 2:{
+        console.log("double click")
+      }
+      default:{
+        break
+      }
+    }
   
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button onClick={handleDoubleClick} id="dblclick-btn">Double click me</button>
     </div>
   )
 }
